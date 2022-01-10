f462e98373783d6e3c560686e12fd91bd5e34903
YagnikBavishi <yagnikbavishi003@gmail.com>
2022-01-10 19:30:41 +0530

    Initial commit

---
+++ practice_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice_ios
+//
+//  Created by Yagnik Bavishi on 10/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
