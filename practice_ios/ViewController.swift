535f366c4f377dc86553d96238da8a4e0dc9f62a
YagnikBavishi <yagnikbavishi003@gmail.com>
2022-01-11 08:06:56 +0530

    str2 added

--- practice_ios/ViewController.swift
+++ practice_ios/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         let str = "hello"
         let str1 = "how are you"
+        let str2 = "I am fine"
     }
 
 
